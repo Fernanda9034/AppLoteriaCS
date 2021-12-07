43c583e200dcef2406c241d7d31a3e73acc3e021
Fernanda9034 <i21050345@monclova.tecnm.mx>
2021-12-06 23:34:22 -0600

    Add project files.

---
+++ AppLoteriaCS/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace AppLoteriaCS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.GBCartas = new System.Windows.Forms.GroupBox();
+            this.pbCarta = new System.Windows.Forms.PictureBox();
+            this.GBHistorial = new System.Windows.Forms.GroupBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.GBCRestantes = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSiguiente = new System.Windows.Forms.Button();
+            this.btnBarajear = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.btnbuenas = new System.Windows.Forms.Button();
+            this.btnTabla = new System.Windows.Forms.Button();
+            this.GBCartas.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCarta)).BeginInit();
+            this.GBHistorial.SuspendLayout();
+            this.GBCRestantes.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // GBCartas
+            // 
+            this.GBCartas.Controls.Add(this.pbCarta);
+            this.GBCartas.Location = new System.Drawing.Point(12, 12);
+            this.GBCartas.Name = "GBCartas";
+            this.GBCartas.Size = new System.Drawing.Size(163, 189);
+            this.GBCartas.TabIndex = 0;
+            this.GBCartas.TabStop = false;
+            this.GBCartas.Text = "Cartas";
+            this.GBCartas.Enter += new System.EventHandler(this.GBCartas_Enter);
+            // 
+            // pbCarta
+            // 
+            this.pbCarta.Location = new System.Drawing.Point(7, 17);
+            this.pbCarta.Name = "pbCarta";
+            this.pbCarta.Size = new System.Drawing.Size(150, 163);
+            this.pbCarta.TabIndex = 0;
+            this.pbCarta.TabStop = false;
+            // 
+            // GBHistorial
+            // 
+            this.GBHistorial.Controls.Add(this.listView1);
+            this.GBHistorial.Location = new System.Drawing.Point(759, 12);
+            this.GBHistorial.Name = "GBHistorial";
+            this.GBHistorial.Size = new System.Drawing.Size(143, 216);
+            this.GBHistorial.TabIndex = 1;
+            this.GBHistorial.TabStop = false;
+            this.GBHistorial.Text = "Historial";
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(6, 16);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(131, 191);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // GBCRestantes
+            // 
+            this.GBCRestantes.Controls.Add(this.label1);
+            this.GBCRestantes.Location = new System.Drawing.Point(184, 12);
+            this.GBCRestantes.Name = "GBCRestantes";
+            this.GBCRestantes.Size = new System.Drawing.Size(108, 54);
+            this.GBCRestantes.TabIndex = 2;
+            this.GBCRestantes.TabStop = false;
+            this.GBCRestantes.Text = "Cartas Restantes";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(7, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnSiguiente
+            // 
+            this.btnSiguiente.Location = new System.Drawing.Point(61, 207);
+            this.btnSiguiente.Name = "btnSiguiente";
+            this.btnSiguiente.Size = new System.Drawing.Size(75, 23);
+            this.btnSiguiente.TabIndex = 3;
+            this.btnSiguiente.Text = "Siguiente Carta";
+            this.btnSiguiente.UseVisualStyleBackColor = true;
+            this.btnSiguiente.Click += new System.EventHandler(this.btnSiguiente_Click);
+            // 
+            // btnBarajear
+            // 
+            this.btnBarajear.Location = new System.Drawing.Point(792, 234);
+            this.btnBarajear.Name = "btnBarajear";
+            this.btnBarajear.Size = new System.Drawing.Size(75, 23);
+            this.btnBarajear.TabIndex = 4;
+            this.btnBarajear.Text = "Barajear";
+            this.btnBarajear.UseVisualStyleBackColor = true;
+            this.btnBarajear.Click += new System.EventHandler(this.btnBarajear_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // btnbuenas
+            // 
+            this.btnbuenas.Location = new System.Drawing.Point(389, 430);
+            this.btnbuenas.Name = "btnbuenas";
+            this.btnbuenas.Size = new System.Drawing.Size(212, 44);
+            this.btnbuenas.TabIndex = 6;
+            this.btnbuenas.Text = "BUENAS";
+            this.btnbuenas.UseVisualStyleBackColor = true;
+            this.btnbuenas.Click += new System.EventHandler(this.btnbuenas_Click);
+            // 
+            // btnTabla
+            // 
+            this.btnTabla.Location = new System.Drawing.Point(12, 263);
+            this.btnTabla.Name = "btnTabla";
+            this.btnTabla.Size = new System.Drawing.Size(163, 27);
+            this.btnTabla.TabIndex = 7;
+            this.btnTabla.Text = "Generar tabla";
+            this.btnTabla.UseVisualStyleBackColor = true;
+            this.btnTabla.Click += new System.EventHandler(this.btnTabla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 507);
+            this.Controls.Add(this.btnTabla);
+            this.Controls.Add(this.btnbuenas);
+            this.Controls.Add(this.btnBarajear);
+            this.Controls.Add(this.btnSiguiente);
+            this.Controls.Add(this.GBCRestantes);
+            this.Controls.Add(this.GBHistorial);
+            this.Controls.Add(this.GBCartas);
+            this.Name = "Form1";
+            this.Text = "Loteria Mexicana";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.GBCartas.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbCarta)).EndInit();
+            this.GBHistorial.ResumeLayout(false);
+            this.GBCRestantes.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox GBCartas;
+        private System.Windows.Forms.GroupBox GBHistorial;
+        private System.Windows.Forms.GroupBox GBCRestantes;
+        private System.Windows.Forms.PictureBox pbCarta;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSiguiente;
+        private System.Windows.Forms.Button btnBarajear;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button btnbuenas;
+        private System.Windows.Forms.Button btnTabla;
+    }
+}
+
---
+++ AppLoteriaCS/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace AppLoteriaCS
+{
+    public partial class Form1 : Form
+    {
+        Stack<int> cartas = new Stack<int>();
+        Random r = new Random();
+        int i = 0;
+        //int c = 0;
+        //int aux;
+        //int[,] tabla = new int[5, 5];
+        //int[] carta = new int[54];
+        //private System.Windows.Forms.PictureBox pictureBox;
+        // 1 arreglo de imagene para la tabla
+        private PictureBox [] tarjetas;
+        private PictureBox[] tablas;
+      
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            //2 arreglo de imagenes
+            tarjetas = new PictureBox[54];
+            tablas = new PictureBox[25];
+            inicializarTabla();
+        }
+
+        // 3 arreglo imagenes declararlo
+        private void inicializarTabla()
+        {
+            int r = 0, c = 0; 
+            for (int i = 0; i < tablas.Length; i++)
+            {
+                tablas[i] = new PictureBox();
+                tablas[i].Location = new System.Drawing.Point(100 + (c * 90), 25 + (r * 125));
+                tablas[i].Name = "ImagenTabla" + i;
+                tablas[i].Size = new System.Drawing.Size(85,120); //tamaño de la imagen
+                tablas[i].TabIndex = 0 + i;
+                tablas[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                tablas[i].TabStop = false;
+                tablas[i].Image = Image.FromFile(@"tarjetas\" + (i + 1) + ".png");        //aun no funciona
+                this.Controls.Add(tablas[i]);
+                c++;
+                if (c==5)
+                {
+                    r++;
+                    c = 0;
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.listView1.View = View.LargeIcon;
+            this.imageList1.ImageSize = new Size(150,160);
+            this.listView1.LargeImageList = this.imageList1;
+            //this.pbTabla.ImageLocation = this.imageList1;
+            //this.pbTabla.LargeImageList = this.imageList1;
+            //this.imageList1.ImageSize = new Size(150, 110);
+            //this.pbTabla.ImageLocation = imageList1; //(NO FUNCIONA, FALTA CORROBORAR LO DE LA TABLA Y NO SABEMOS SI EL 
+            // ARREGLO DE CARTA PUEDA FUNCIONAR EN ALGO CHTM TE ODIO PROGRAM
+        }
+
+        private void btnSiguiente_Click(object sender, EventArgs e)
+        {
+            label1.Text = (54 - cartas.Count()).ToString();
+            bool aux = false;
+            if (cartas.Count() == 54)
+            {
+                aux = true;
+                MessageBox.Show("Son todas las cartas","AVISO",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+
+            while (!aux)
+            {
+                int num = r.Next(1, 54);
+                if (!cartas.Contains(num))
+                {
+                    pbCarta.Image = Image.FromFile(@"C:\Users\Usuario\Pictures\Baraja Loteria\"+num+".png");
+                    pbCarta.SizeMode = PictureBoxSizeMode.StretchImage;
+                    cartas.Push(num);
+                    this.imageList1.Images.Add(Image.FromFile(@"C:\Users\Usuario\Pictures\Baraja Loteria\" + num + ".png"));
+                    ListViewItem item = new ListViewItem();
+                    item.ImageIndex = i;
+                    this.listView1.Items.Add(item);
+                    aux = true;
+                    i++;
+                }
+            }
+        }
+
+        private void btnBarajear_Click(object sender, EventArgs e)
+        {
+            cartas.Clear();
+            imageList1.Images.Clear();
+            listView1.Items.Clear();
+            pbCarta.Image = null;
+            i = 0;
+            label1.Text = "";
+           
+        }
+        private void GBCartas_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnbuenas_Click(object sender, EventArgs e)
+        {
+            SystemSounds.Beep.Play();
+            //SoundPlayer Player = new SoundPlayer();
+            //Player.SoundLocation = "C:/Users/Usuario/Documents/Sonar.wav";
+            //public void Play(string "C:/Users/Usuario/Documents/Sonar.wav");
+            //SoundPlayer.Play();
+
+        }
+
+        private void btnTabla_Click(object sender, EventArgs e)
+        {
+
+        //    for (int i = 0; i < tabla.GetLength(0); i++)
+        //    {
+        //        for (int j = 0; j < tabla.GetLength(0); j++)
+        //        {
+        //            tabla[i, j] = carta[c++];
+        //        }
+        //    }
+        //    //int ar, ac;
+        //    //for (int i = 0; i < tabla.GetLength(0); i++)
+        //    {
+        //        for (int j = 0; j < tabla.GetLength(0); j++)
+        //        {
+        //            ar = r.Next(5);
+        //            ac = r.Next(5);
+        //            aux = tabla[i, j];
+        //            tabla[i, j] = tabla[ar, ac];
+        //            tabla[ar, ac] = aux;
+        //        }
+        //    }
+        }
+    }
+}
